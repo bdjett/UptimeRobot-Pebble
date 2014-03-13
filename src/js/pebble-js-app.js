f728116f3ec3ec84a61dd233610d4799dbace62e
bdjett <bdjett@me.com>
2014-03-13 13:34:41 -0400

    Started working on JS

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,5 +1,66 @@
-Pebble.addEventListener("ready",
-    function(e) {
-        console.log("Hello world! - Sent from your javascript application.");
+/*******************
+ * pebble-js-app.js
+ *
+ * JavaScript for UptimeRobot
+ *
+ * Created by: Brian Jett
+ */
+
+var maxAppMessageBuffer = 100;
+var maxAppMessageTries = 3;
+var appMessageRetryTimeout = 3000;
+var appMessageTimeout = 100;
+var httpTimeout = 12000;
+var appMessageQueue = [];
+var isNewList = false;
+
+// User's current monitors
+var monitors = [];
+var monitorIds = [];
+
+// getMonitors
+// -----------
+//
+// gets all of the monitors associated with key
+var getMonitors = function() {
+    console.log("Getting monitors");
+    var key = localStorage['api_key'].toString();
+    var req = new XMLHttpRequest();
+    if (key) {
+        var requestUrl = "http://api.uptimerobot.com/getMonitors?apiKey=" + key +
+                            "&logs=0&alertContacts=0&responseTimes=1&format=json&noJsonCallback=1";
+        console.log(requestUrl);
+        req.open('GET', requestUrl, true);
+        req.onload = function(e) {
+            if (req.readyState == 4) {
+                if (req.status == 200) {
+                    if (req.responseText) {
+                        var response = JSON.parse(req.responseText);
+                        response["monitors"].forEach(function(element, index, array) {
+                            monitors[element["id"]] = element;
+                            monitorIds.push(element["id"]);
+                        });
+                    }
+                }
+            }
+        };
+        req.ontimeout = function() {
+            console.log("HTTP request timed out");
+            sendError("Request timed out!");
+        };
+        req.onerror = function() {
+            console.log("HTTP request returned error");
+            sendError("Failed to connect!");
+        };
+        req.send(null);
     }
-);
+}
+
+// getStatus
+// ---------
+//
+// takes a monitor id and returns the status
+var getStatus = function(monitorId) {
+    console.log("Getting status of monitorId: " + monitorId);
+    
+}
