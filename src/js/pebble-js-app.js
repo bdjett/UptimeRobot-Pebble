a77c1c7b33362131def1db74446ff6af0d421753
bdjett <bdjett@me.com>
2014-03-13 14:14:14 -0400

    All JS functions

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -37,12 +37,20 @@ var getMonitors = function() {
                     if (req.responseText) {
                         var response = JSON.parse(req.responseText);
                         response["monitors"].forEach(function(element, index, array) {
-                            monitors[element["id"]] = element;
-                            monitorIds.push(element["id"]);
+                            monitors[parseInt(element["id"])] = element;
+                            monitorIds.push(parseInt(element["id"]));
+                            var friendlyName = element["friendlyname"];
+                            var url = element["url"];
+                            var status = element["status"];
+                            appMessageQueue.push({'message': {
+                                                    'friendlyName': friendlyName,
+                                                    'url': url,
+                                                    'status': status}});
                         });
                     }
                 }
             }
+            sendAppMessage();
         };
         req.ontimeout = function() {
             console.log("HTTP request timed out");
@@ -62,5 +70,72 @@ var getMonitors = function() {
 // takes a monitor id and returns the status
 var getStatus = function(monitorId) {
     console.log("Getting status of monitorId: " + monitorId);
-    
+    if (monitors[parseInt(monitorId)]) {
+        var monitor = monitors[parseInt(monitorId)];
+        var friendlyName = monitor["friendlyname"];
+        var url = monitor["url"];
+        var status = monitor["status"];
+        var uptimeRatio = monitor["alltimeuptimeratio"];
+        appMessageQueue.push({'message':
+                                {'friendlyName': friendlyName.toString(),
+                                 'url': url.toString(),
+                                 'status': status.toString(),
+                                 'uptimeRatio': uptimeRatio.toString()}});
+        sendAppMessage();
+    } else {
+        sendError("Monitor does not exist.");
+    }
 }
+
+// sendError
+// ---------
+//
+// returns error to pebble
+var sendError = function(error) {
+    Pebble.showSimpleNotificationOnPebble("Error!", error.toString());
+}
+
+// sendAppMessage
+// --------------
+//
+// sends app message queue to pebble
+var sendAppMessage = function() {
+    if (appMessageQueue.length > 0) {
+        var currentAppMessage = appMessageQueue[0];
+        currentAppMessage.numTries = currentAppMessage.numTries || 0;
+        currentAppMessage.transactionId = currentAppMessage.transactionId || -1;
+
+        if (currentAppMessage.numTries < maxAppMessageTries) {
+            console.log("Sending message");
+            Pebble.sendAppMessage(
+                currentAppMessage.message,
+                function(e) {
+                    appMessageQueue.shift();
+                    setTimeout(function() {
+                        sendAppMessage();
+                    }, appMessageTimeout);
+                }, function(e) {
+                    console.log("Faled sending AppMessage for transactionId: " + e.data.transactionId + ". Error: " + e.data.error.message);
+                    appMessageQueue[0].transactionId = e.data.transactionId;
+                    appMessageQueue[0].numTries++;
+                    setTimeout(function() {
+                        sendAppMessage();
+                    }, appMessageRetryTimeout);
+                }
+            );
+        } else {
+            console.log("Faled sending AppMessage after multiple attemps for transactionId: " + currentAppMessage.transactionId + ". Error: None. Here's the message: " + JSON.stringify(currentAppMessage.message));
+        }
+    }
+}
+
+Pebble.addEventListener("appmessage", function(e) {
+    if (e.payload.getStatus) {
+        getStatus(e.payload.monitorId);
+    }
+});
+
+Pebble.addEventListener("ready", function(e) {
+    localStorage["api_key"] = "u133021-5a86707198f9c60830ce4275";
+    getMonitors();
+});
