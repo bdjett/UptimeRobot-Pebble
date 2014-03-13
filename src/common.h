8d231adc02cb6c5df4c30f2279cfe6d3bbed4a78
bdjett <bdjett@me.com>
2014-03-13 14:13:15 -0400

    Added common header

---
+++ src/common.h
@@ -0,0 +1,20 @@
+#pragma once
+
+typedef struct {
+	char id[25];
+	char name[128];
+	char url[128];
+	char status[2];
+	char uptime[4];
+	int index;
+} Monitor;
+
+enum {
+	MONITOR_ID = 0x0,
+	MONITOR_NAME = 0x1,
+	MONITOR_URL = 0x2,
+	MONITOR_STATUS = 0x3,
+	MONITOR_UPTIME = 0x4,
+	MONITOR_INDEX = 0x5,
+	MONITOR_GET_STATUS = 0x6,
+};
